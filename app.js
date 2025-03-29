384201b642133d4ad50c537448910bc853fcd7ad
Haaj <91592261+ImranMusaAyomide@users.noreply.github.com>
2025-03-30 00:29:17 +0100

    Added product data, features section, and product details to the app, updated styles for the new elements.

--- app.js
+++ app.js
@@ -1,8 +1,69 @@
- const wrapper = document.querySelector(".sliderWrapper");
+const wrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
 
+
+const products = [
+  {
+      id: 1,
+      title: "Air Force",
+      price: 119,
+      colors: [
+          {
+              code: "black",
+              img: "./img/sneakers/(1).png",
+          },
+          {
+              code: "black",
+              img: "./img/sneakers/(2).png",
+          },
+          {
+              code: "darkblue",
+              img: "./img/sneakers/(3).png",
+          },
+          {
+              code: "lightblue",
+              img: "./img/sneakers/(4).png",
+          },
+          {
+              code: "white",  
+              img: "./img/sneakers/(5).png",
+          },
+          {
+              code: "black",
+              img: "./img/sneakers/(5).png",
+          },
+      ],
+  },
+  {
+      id: 2,
+      title: "Air Jordan",
+      price: 149,
+      colors: [
+          {
+              code: "black",
+              img: "./img/jordan.png",
+          },
+      ],
+  },
+  {
+      id: 5,
+      title: "Hippie",
+      price: 99,
+      colors: [
+          {
+              code: "black",
+              img: "./img/hippie2:png"
+          },
+      ],
+  },
+];
+
+let choosenProduct = products[0]
 menuItems.forEach((item, index) => {
   item.addEventListener("click", () =>  {
+    //change the current slide
     wrapper.style.transform=`translateX(${-100 * index}vw)`;
   });
+  //change the choosen products
+  choosenProduct = products[index]
 });
---
+++ img/api.js
@@ -0,0 +1,56 @@
+
+const products = [
+    {
+        id: 1,
+        title: "Air Force",
+        price: 119,
+        colors: [
+            {
+                code: "black",
+                img: "./img/air.png",
+            },
+            {
+                code: "black",
+                img: "./img/air.png",
+            },
+            {
+                code: "darkblue",
+                img: "./img/air.png",
+            },
+            {
+                code: "lightblue",
+                img: "./img/air.png",
+            },
+            {
+                code: "white",  
+                img: "./img/air.png",
+            },
+            {
+                code: "black",
+                img: "./img/air.png",
+            },
+        ],
+    },
+    {
+        id: 2,
+        title: "Air Jordan",
+        price: 149,
+        colors: [
+            {
+                code: "black",
+                img: "./img/jordan.png",
+            },
+        ],
+    },
+    {
+        id: 5,
+        title: "Hippie",
+        price: 99,
+        colors: [
+            {
+                code: "black",
+                img: "./img/hippie2:png"
+            },
+        ],
+    },
+];
